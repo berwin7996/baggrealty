3be2ebdeb5e90d22de8f3996c697e4154caed55e
berwin7996 <berwin7996@gmail.com>
2015-03-18 17:55:15 -0500

    init

---
+++ hello/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Greeting
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Welcome to Bagg Realty!')
+
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, 'db.html', {'greetings': greetings})
+
+def index(request):
+    return HttpResponse('Welcome to Bagg Realty!')
+def db(request):
+    greeting = Greeting()
+    greeting.save() ...
+    greetings = Greeting.objects.all()
+    return render(request, 'db.html', {'greetings': greetings})
