c470fcc0fb63c633681a6d59dc26681e3e7f1fd6
berwin7996 <berwin7996@gmail.com>
2015-04-18 11:23:39 -0500

    try with tzinfo removed

--- realty_management/management/commands/check_lease.py
+++ realty_management/management/commands/check_lease.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		properties = Property.objects.all()
 		for p in properties:
-			todayplusthirty = datetime.now() + timedelta(days=30)
+			todayplusthirty = datetime.now().replace(tzinfo=None) + timedelta(days=30)
 	        contracts = LivesIn.objects.filter(unit_number__in=p.get_owned_units())
 	        for c in contracts:
 	            if todayplusthirty > c.lease_end:
