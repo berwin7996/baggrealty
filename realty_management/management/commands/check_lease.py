8d510b7ecea16f023e79d4d47ad208d1d5f23e1d
berwin7996 <berwin7996@gmail.com>
2015-04-18 12:26:30 -0500

    replacing tabs with spaces

--- realty_management/management/commands/check_lease.py
+++ realty_management/management/commands/check_lease.py
@@ -3,17 +3,17 @@ from realty_management.models import Property, LivesIn
 from datetime import datetime, timedelta
 
 class Command(BaseCommand):
-	def handle(self, *args, **options):
-		properties = Property.objects.all()
-		for p in properties:
-			print('checking contracts for ' + p.address)
-			todayplusthirty = datetime.now() + timedelta(days=30)
-	        contracts = LivesIn.objects.filter(unit_number__in=p.get_owned_units())
-	        for c in contracts:
-	        	print('time right now + 30 days: ' + todayplusthirty.replace(tzinfo=None))
-	        	print('lease end time: ' + c.lease_end.replace(tzinfo=None))
-	            if todayplusthirty.replace(tzinfo=None) > c.lease_end.replace(tzinfo=None):
-	                #end email if date is 30 before end of lease
-	                print(p.address, c.unit_number)
-	                message = send_mail('LEASE EXPIRING SOON', '', 'baggrealty@gmail.com', ['baggrealty@gmail.com'], fail_silently=False)
-	        print('========================')
+    def handle(self, *args, **options):
+        properties = Property.objects.all()
+        for p in properties:
+            print('checking contracts for ' + p.address)
+            todayplusthirty = datetime.now() + timedelta(days=30)
+            contracts = LivesIn.objects.filter(unit_number__in=p.get_owned_units())
+            for c in contracts:
+                print('time right now + 30 days: ' + todayplusthirty.replace(tzinfo=None))
+                print('lease end time: ' + c.lease_end.replace(tzinfo=None))
+                if todayplusthirty.replace(tzinfo=None) > c.lease_end.replace(tzinfo=None):
+                    #end email if date is 30 before end of lease
+                    print(p.address, c.unit_number)
+                    message = send_mail('LEASE EXPIRING SOON', '', 'baggrealty@gmail.com', ['baggrealty@gmail.com'], fail_silently=False)
+            print('========================')
