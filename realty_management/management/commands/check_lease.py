94da71e37b482c6836f7cf547c54356da6c5ab9f
berwin7996 <berwin7996@gmail.com>
2015-04-18 12:18:49 -0500

    more debuggery code for figuring out time comparisons to send email

--- realty_management/management/commands/check_lease.py
+++ realty_management/management/commands/check_lease.py
@@ -10,7 +10,10 @@ class Command(BaseCommand):
 			todayplusthirty = datetime.now() + timedelta(days=30)
 	        contracts = LivesIn.objects.filter(unit_number__in=p.get_owned_units())
 	        for c in contracts:
+	        	print('time right now + 30 days: ' + todayplusthirty.replace(tzinfo=None))
+	        	print('lease end time: ' + c.lease_end.replace(tzinfo=None))
 	            if todayplusthirty.replace(tzinfo=None) > c.lease_end.replace(tzinfo=None):
 	                #end email if date is 30 before end of lease
 	                print(p.address, c.unit_number)
 	                message = send_mail('LEASE EXPIRING SOON', '', 'baggrealty@gmail.com', ['baggrealty@gmail.com'], fail_silently=False)
+	        print('========================')
