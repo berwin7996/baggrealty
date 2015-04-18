9a3f2584b3d91881ccbbc605763ba5475e3a068e
berwin7996 <berwin7996@gmail.com>
2015-04-18 11:32:27 -0500

    try with tzinfo removed

--- realty_management/management/commands/check_lease.py
+++ realty_management/management/commands/check_lease.py
@@ -6,9 +6,9 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		properties = Property.objects.all()
 		for p in properties:
-			todayplusthirty = datetime.now().replace(tzinfo=None) + timedelta(days=30)
+			todayplusthirty = datetime.now() + timedelta(days=30)
 	        contracts = LivesIn.objects.filter(unit_number__in=p.get_owned_units())
 	        for c in contracts:
-	            if todayplusthirty > c.lease_end:
+	            if todayplusthirty.replace(tzinfo=None) > c.lease_end.replace(tzinfo=None):
 	                #end email if date is 30 before end of lease
 	                message = send_mail('LEASE EXPIRING SOON', '', 'baggrealty@gmail.com', ['baggrealty@gmail.com'], fail_silently=False)
