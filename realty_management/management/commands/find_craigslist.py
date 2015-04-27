53323c718967f5817655dbfeb46a8535fb4910a5
berwin7996 <berwin7996@gmail.com>
2015-04-27 01:42:57 -0500

    craigslist scraping works. working on querying databse to get interesting results

--- realty_management/management/commands/find_craigslist.py
+++ realty_management/management/commands/find_craigslist.py
@@ -1,31 +1,104 @@
 from django.core.management import BaseCommand, CommandError
 from realty_management.models import Unit
 
+
 import requests
 import lxml
-from lxml import html
+from lxml import html, etree
+from lxml.cssselect import CSSSelector
+import time, datetime
 
-apartments = {}
-
+apartment_details = []
+# scrape_url = 'http://chambana.craigslist.org/search/apa'
+scrape_url = 'http://chambana.craigslist.org/'
 class Command(BaseCommand):
-     help = 'Scrapes the sites for new dockets'
-     def handle(self, *args, **options):
-         self.stdout.write('\nScraping started at %s\n' % str(datetime.datetime.now()))
-         craigslist = {'chambana': 'http://chambana.craigslist.org/search/apa'}
-         for list, url in craigslist.iteritems():
-             self.stdout.write('Scraping url: %s\n' % url)
-             r = requests.get(url)
-             root = lxml.html.fromstring(r.content)
-             # 
-             for row in root.cssselect('p[class="row"] a'):
-             	print row
+    help = 'Scrapes the sites for new dockets'
+    def handle(self, *args, **options):
+        self.stdout.write('\nScraping started at %s\n' % str(datetime.datetime.now()))
+        r = requests.get(scrape_url + 'search/apa')
+        # build the DOM Tree
+        tree = lxml.html.fromstring(r.text)
+        # construct a CSS Selector
+        sel = CSSSelector('p.row a')
+        # Apply the selector to the DOM tree.
+        results = sel(tree)
+        #print results
+        # get the text out of all the results
+        data = [str(result.get('href')) for result in results]
+        data = [str(scrape_url + result) for result in data if result != 'None']
+        
+        populate_details(data)
+
+        # average price of all the apartments
+        prices = [float(apt['cost']) for apt in apartment_details]
+        avg_price = sum(prices)/len(prices)
+        print avg_price
+
+        # Unit.objects.raw('SELECT ')
 
  
- now = time.gmtime(time.time())
+now = time.gmtime(time.time())
  
- def convertTime(t):
-     """Converts times in format HH:MMPM into seconds from epoch (but in CST)"""
-     convertedTime = time.strptime(t + ' ' + str(now.tm_mon) + ' ' + str(now.tm_mday) + ' ' + str(now.tm_year), "%I:%M%p %m %d %Y")
-     return time.mktime(convertedTime)
-     # This used to add 5 * 60 * 60 to compensate for CST
- 
+def convertTime(t):
+   """Converts times in format HH:MMPM into seconds from epoch (but in CST)"""
+   convertedTime = time.strptime(t + ' ' + str(now.tm_mon) + ' ' + str(now.tm_mday) + ' ' + str(now.tm_year), "%I:%M%p %m %d %Y")
+   return time.mktime(convertedTime)
+   # This used to add 5 * 60 * 60 to compensate for CST
+
+def populate_details(data):
+	# go through links to get info
+    for index, link in enumerate(list(set(data))):
+
+        # debugging
+        if index > 10:
+            break
+
+        print 'scraping ' + link
+        r = requests.get(link)
+        tree = lxml.html.fromstring(r.text)
+        #print lxml.html.tostring(tree)
+        sel = CSSSelector('p.attrgroup span')
+        results = sel(tree)
+
+        match = results[0]
+        #print lxml.html.tostring(match)
+        br, ba = gettext(lxml.html.tostring(match))
+        #print 'br', br, 'ba', ba
+        #break
+
+        sel = CSSSelector('span.price')
+        results = sel(tree)
+        match = results[0]
+        price = lxml.html.tostring(match)[21:-8]
+        print 'price', price
+        if price.isdigit():
+        	apartment_details.append({'br':br, 'ba':ba, 'cost':price})
+        else:
+        	print 'not a digit...'
+
+# for br ba
+def gettext(source):
+    '''
+    <span><b>1</b>BR / <b>1</b>Ba</span> 
+    '''
+    # print type(source)
+    source = etree.fromstring(source)
+    # print etree.tostring(source)
+    etree.strip_tags(source, 'b', 'span')
+    newstr = etree.tostring(source)
+    br = newstr.split('/')[0].split('>')[1]
+    ba = newstr.split('/')[1].split('<')[0]
+
+    br = br[:-2] # remove Br
+    ba = ba[:-2] # remove Ba
+
+    try:
+        br = int(br)
+    except:
+        br = 1
+    try:
+        ba = int(ba)
+    except:
+        ba = 1
+
+    return br, ba
