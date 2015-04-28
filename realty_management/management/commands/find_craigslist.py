5f11fc8b23ed56c598639acd14258de531b8395a
berwin7996 <berwin7996@gmail.com>
2015-04-27 21:36:26 -0500

    changed craigslist search to get cost per sqft

--- realty_management/management/commands/find_craigslist.py
+++ realty_management/management/commands/find_craigslist.py
@@ -31,10 +31,13 @@ class Command(BaseCommand):
 
         
         # get avg cost per sq in
-        cost_sum = Unit.objects.raw('SELECT SUM("realty_management_unit"."rent") AS avg FROM "realty_management_unit"')
-        cost_sum = cost_sum[0]
-        sqft_sum = Unit.objects.raw('SELECT SUM("realty_management_unit"."sq_ft") AS avg FROM "realty_management_unit"')
-        sqft_sum = sqft_sum[0]
+        # cost_sum = Unit.objects.raw('SELECT SUM("realty_management_unit"."rent") AS avg FROM "realty_management_unit"')
+        # distinct_queryset = MyModel.objects.filter(reverse_relationship__icontains='foo').distinct()
+
+        cost_sum = Unit.objects.aggregate(Sum('rent'))
+        # sqft_sum = Unit.objects.raw('SELECT SUM("realty_management_unit"."sq_ft") AS avg FROM "realty_management_unit"')
+        # sqft_sum = sqft_sum[0]
+        sqft_sum = Unit.objects.aggregate(Sum('sq_ft'))
         my_avg_cost_sqft = cost_sum / sqft_sum
         print 'mine: ' + str(my_avg_cost_sqft)
 
